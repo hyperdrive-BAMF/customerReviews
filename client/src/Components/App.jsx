c7079c30e7ee04597a7ce44f9913bc257a26d93a
Zane Mattingly <z@matting.ly>
2018-05-31 19:48:07 -0700

    setup: adding linting

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,13 +1,10 @@
 const React = require('react');
 
-var App = (props) => {
-  console.log('dafuq');
-  return (
-    <div id="app">
-      <h1>Hello World!</h1>
-      <p>The Audience is Listening...</p>
-    </div>
-  );
-};
+const App = () => (
+  <div id="app">
+    <h1>Hello World!</h1>
+    <p>The Audience is Listening...</p>
+  </div>
+);
 
 export default App;
